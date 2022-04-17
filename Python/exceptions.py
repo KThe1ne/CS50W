4933a169a3be1e82a6eec5785c5e23fa9855d5e9
Kamoi Gilkes <gilkeskamoi@gmail.com>
2022-04-17 00:40:47 -0400

    Value Error

--- Python/exceptions.py
+++ Python/exceptions.py
@@ -3,15 +3,19 @@ from tkinter import Y
 
 import sys
 
-x = int(input("x: "))
-y = int(input("y: "))
-
+try:
+    x = int(input("x: "))
+    y = int(input("y: "))
+except ValueError:
+    print("Not int")
+    sys.exit(1)
 
 try:
     result = x/y
 except ZeroDivisionError:
     print("Error: Cannot divide by 0.")
     sys.exit(1)
-
+except ValueError:
+    print("Not an int")
 print(f"{x} / {y} = {result}")
 
