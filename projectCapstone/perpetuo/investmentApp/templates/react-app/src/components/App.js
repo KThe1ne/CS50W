be351bf3dfdde46bbd962a70c6ae646acb9d5169
KThe1ne <gilkeskamoi@gmail.com>
2022-07-04 23:52:42 -0400

    React + Tailwind Integration Complete!!!!

--- projectCapstone/perpetuo/investmentApp/templates/react-app/src/components/App.js
+++ projectCapstone/perpetuo/investmentApp/templates/react-app/src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {createRoot} from 'react-dom/client';
+import '../../dist/output.css';
 
 function App() {
     return (
        <div>
-            <div className="header">
-                Testing React!!!
+            <div className="text-3xl font-bold underline">
+                Testing React
             </div>
        </div> 
     )
